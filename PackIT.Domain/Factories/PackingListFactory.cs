60bb3786f32cd5d320577fd101b4ee4818e15917
Sandeep <sandeepchopra7@gmail.com>
2023-10-26 16:26:03 -0400

    implment packing list factory

---
+++ PackIT.Domain/Factories/PackingListFactory.cs
@@ -0,0 +1,19 @@
+﻿using PackIT.Domain.Consts;
+using PackIT.Domain.Entities;
+using PackIT.Domain.Factories;
+using PackIT.Domain.ValueObjects;
+
+namespace PackIT.Domain;
+
+public class PackingListFactory : IPackingListFactory
+{
+  public PackingList Create(PackingListId id, PackingListName name, Localization localization)
+  {
+    throw new NotImplementedException();
+  }
+
+  public PackingList CreateWithDefaultItems(PackingListId id, PackingListName name, TravelDays days, Gender gender, Localization localization)
+  {
+    throw new NotImplementedException();
+  }
+}
---
+++ PackIT.Domain/ValueObject/TravelDays.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using PackIT.Domain.Exceptions;
+
+namespace PackIT.Domain.ValueObjects;
+
+public record TravelDays
+{
+  public ushort Value { get; private set; }
+  public TravelDays(ushort value)
+  {
+    if (value is 0 or > 100)
+    {
+      throw new InvalidTravelDaysException(value);
+    }
+    Value = value;
+  }
+
+  public static implicit operator ushort(TravelDays days) => days.Value;
+
+  public static implicit operator TravelDays(string days) => new(days);
+
+}
