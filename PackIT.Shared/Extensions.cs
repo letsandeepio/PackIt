6f8c1568026cb9f025dcbc7c5d8c1c5bb1930e39
Sandeep <sandeepchopra7@gmail.com>
2023-10-27 11:23:34 -0400

    implement extensions for automatic registration of command handlers

--- PackIT.Shared/Extensions.cs
+++ PackIT.Shared/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using PackIT.Shared.Abstractions.Commands;
 using PackIT.Shared.Commands;
@@ -9,7 +10,13 @@ public static class Extensions
 {
   public static IServiceCollection AddCommands(this IServiceCollection services)
   {
+
+    var assembly = Assembly.GetCallingAssembly();
+
     services.AddSingleton<ICommandDispatcher, InMemoryCommandDispatcher>();
+
+    services.Scan(s => s.FromAssemblies(assembly).AddClasses(c => c.AssignableTo(typeof(ICommandHandler<>))).AsImplementedInterfaces().WithScopedLifetime());
+
     return services;
   }
 }
