a7a88c6c5ed19faa7cea94465db73a682d583c81
Sandeep <sandeepchopra7@gmail.com>
2023-10-27 09:56:45 -0400

    implement low temperature policy

---
+++ PackIT.Domain/Policies/Temperature/LowTemperaturePolicy.cs
@@ -0,0 +1,16 @@
+﻿using PackIT.Domain.Policies;
+
+namespace PackIT.Domain.Policies.Temperature;
+
+internal class LowTemperaturePolicy : IPackingItemsPolicy
+{
+  public IEnumerable<PackingItem> GenerateItems(PolicyData data) => new List<PackingItem> {
+    new(name: "Winter Hat", quantity: 1),
+    new(name: "Scarf", quantity: 2),
+    new(name: "Gloves", quantity: 3),
+    new(name: "Hoodie", quantity: 3),
+    new(name: "Warm Jacket", quantity: 3),
+  };
+
+  public bool isApplicable(PolicyData data) => data.temperature < 10D;
+}
