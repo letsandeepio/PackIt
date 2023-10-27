7d7ac42d2696e29f5ec787704063c832c1c023b5
Sandeep <sandeepchopra7@gmail.com>
2023-10-27 09:51:26 -0400

    implement female gender policy

---
+++ PackIT.Domain/Policies/Gender/FemaleGenderPolicy.cs
@@ -0,0 +1,14 @@
+﻿using PackIT.Domain.Policies;
+
+namespace PackIT.Domain;
+
+internal class FemaleGenderPolicy : IPackingItemsPolicy
+{
+  public IEnumerable<PackingItem> GenerateItems(PolicyData data) => new List<PackingItem> {
+    new(name: "Lipstick", quantity: 1),
+    new(name: "Powder", quantity: 2),
+    new(name: "Eyeliner", quantity: 3),
+  };
+
+  public bool isApplicable(PolicyData data) => data.gender is Consts.Gender.Female;
+}
