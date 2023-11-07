02a82666dbd0e0ea80e4f4a61d6fb08c2b0b4127
Sandeep <sandeepchopra7@gmail.com>
2023-11-07 18:02:49 -0500

    add readconfiguration

--- PackIT.Infrastructure/EF/Models/LocalizationReadModel.cs
+++ PackIT.Infrastructure/EF/Models/LocalizationReadModel.cs
@@ -4,4 +4,20 @@ internal class LocalizationReadModel
 {
   public string City { get; set; }
   public string Country { get; set; }
+
+  public static LocalizationReadModel Create(string value)
+  {
+    var splitLocalization = value.Split(",");
+    return new LocalizationReadModel
+    {
+      City = splitLocalization.First(),
+      Country = splitLocalization.Last()
+    };
+  }
+
+
+  public override string ToString()
+  {
+    return $"{City}, {Country}";
+  }
 }
