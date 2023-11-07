0cd91a26e31b66a2cc3ecb9bc0ed4f8374c56577
Sandeep <sandeepchopra7@gmail.com>
2023-11-07 18:38:33 -0500

    add postgres connectionstring and shared packages

---
+++ PackIT.Shared/Options/Extensions.cs
@@ -0,0 +1,13 @@
+﻿namespace PackIT.Shared.Options;
+using Microsoft.Extensions.Configuration;
+
+public static class Extensions
+{
+  public static TOptions GetOptions<TOptions>(this IConfiguration configuration, string sectionName) where TOptions : new()
+  {
+    var options = new TOptions();
+    configuration.GetSection(sectionName).Bind(options);
+
+    return options;
+  }
+}
