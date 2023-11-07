b2b4887cf9791378d226d84bf0021b790135ec77
Sandeep <sandeepchopra7@gmail.com>
2023-11-07 18:49:37 -0500

    add Extensions to EF and Infrastructure to register the db context

---
+++ PackIT.Infrastructure/EF/Options/PostgresOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace PackIT.Infrastructure.EF.Options;
+
+public class PostgresOptions
+{
+  public string ConnectionString { get; set; }
+}
--- PackIT.Infrastructure/Queries/Extensions.cs
+++ PackIT.Infrastructure/Queries/Extensions.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PackIT.Infrastructure.EF;
+using PackIT.Infrastructure.EF.Options;
+using PackIT.Shared.Options;
 using PackIT.Shared.Queries;
 
 namespace PackIT.Infrastructure;
 
 public static class Extensions
 {
-  public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+  public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
   {
+
+    services.AddPostgres(configuration);
+
     services.AddQueries();
+
     return services;
   }
 
