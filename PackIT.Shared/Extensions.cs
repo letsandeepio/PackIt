1ce69adb6e7210d4a2cb78f829611029dd475e4e
Sandeep <sandeepchopra7@gmail.com>
2023-11-08 19:12:48 -0500

    add automatic registration of dbcontexts

---
+++ PackIT.Shared/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PackIT.Shared;
+
+public static class Extensions
+{
+  public static IServiceCollection AddShared(this IServiceCollection services)
+  {
+
+    services.AddHostedService<AppInitializer>();
+    return services;
+  }
+}
