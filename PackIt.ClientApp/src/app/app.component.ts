32965199a34ceec7e1aa35fdf1aeee1587602f8f
Sandeep <sandeepchopra7@gmail.com>
2023-11-16 19:49:44 -0500

    minor fixes

--- PackIt.ClientApp/src/app/app.component.ts
+++ PackIt.ClientApp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { AppTitleBarComponent } from './app-title-bar/app-title-bar.component';
+import { AppTitleBarComponent } from './components/app-title-bar/app-title-bar.component';
 
 @Component({
   selector: 'app-root',
